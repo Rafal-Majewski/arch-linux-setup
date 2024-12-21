2ec083c5222062c2fb369808a98d35829065a3c6
Rafał Majewski <goodheropl@gmail.com>
2024-12-21 21:10:24 +0100

    feature: Add displaying the battery status to the bar

---
+++ assets/.config/i3/bar/src/message-segment-computer-implementations/battery-status/computing-status/computing-charging-state/computeBatteryStatusChargingState.ts
@@ -0,0 +1,17 @@
+import type { BatteryStatusChargingState } from "../../status/charging-state/BatteryStatusChargingState.ts";
+
+export function computeBatteryStatusChargingState(
+	statusCommandOutput: "Charging\n" | "Discharging\n" | "Full\n",
+): BatteryStatusChargingState {
+	switch (statusCommandOutput) {
+		case "Charging\n": {
+			return "charging";
+		}
+		case "Discharging\n": {
+			return "discharging";
+		}
+		case "Full\n": {
+			return "full";
+		}
+	}
+}
